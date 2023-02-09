8514b219478fb46f5985667b2e0289ee20e12544
marcusmarchiori <marcus_marchiori@hotmail.com>
2023-02-09 16:58:21 -0300

    Primeiro commit

---
+++ src/main/java/com/desafio/chavepix/ChavePixApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.chavepix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavePixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavePixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/chavepix/ChavePixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.chavepix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavePixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
