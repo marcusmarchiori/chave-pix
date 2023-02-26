0e6c6cb4f95418668c9175d91fea8360c479e5b7
marcusmarchiori <marcus_marchiori@hotmail.com>
2023-02-26 12:48:43 -0300

    Config e Migration

---
+++ src/main/resources/db/migration/V5__insert_account.sql
@@ -0,0 +1,2 @@
+INSERT INTO account (bank_name, type_account, num_account, balance_account, type_key, key_pix)
+VALUES ('Pagseguro', 'PF', 1234, 300.00, 'EMAIL','marcus@tqi.com.br');
---
+++ src/main/resources/db/migration/V6__alter_column_account.sql
@@ -0,0 +1,2 @@
+INSERT INTO account (bank_name, type_account, num_account, balance_account, type_key, key_pix)
+VALUES ('Pagseguro', 'PJ', 12345, 100.00, 'EMAIL','marcus@tqi.com.br');
